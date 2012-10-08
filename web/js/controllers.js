c7eed96f7a54c4ab468e11a64d10eba068816636
Jens Augustsson <jens@augustsson.eu>
2012-10-09 01:42:01 +0200

    Now markers are rendered in AngularJS

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -7,8 +7,23 @@ function GMapCtrl($scope, $http) {
     $scope.person = { quote:"\"I put on my robe and wizard hat\" - BloodNinja", img:"img/vidvattnet.jpg" };
     
     $http.get('family.json').success(function(data, status, headers, config) {
-       $scope.persons = data.persons; 
-    });
+       $scope.persons = data.persons;
+       $scope.places = data.places;
+       $scope.map = null;
+    
+        if (GBrowserIsCompatible()) {
+            $scope.map = new GMap2(document.getElementById("map"));
+            $scope.map.setCenter(new GLatLng(57.84701, 12.36555), 11);
+    		$scope.map.setMapType(G_HYBRID_MAP);
+    		$scope.map.addControl(new GSmallMapControl());				
+    
+    		$scope.persons.each(function(person) {
+    			if (person.placeId && $scope.places[person.placeId]) {
+    				 var marker = createGMarker(person, $scope.places[person.placeId]);
+    				 $scope.map.addOverlay(marker);
+    			}                    
+    		});        
+        }
     
     /*
     $scope.persons = [
@@ -16,40 +31,7 @@ function GMapCtrl($scope, $http) {
         { id:2, name:'Adam', quote:'\"I put on my robe and wizard hat\" - BloodNinja', img:'img/vidvattnet.jpg' }];
      */   
         
-    $scope.map = null;
-    $scope.markers = {};
-    $scope.family = {};
- /*   										
-	if (GBrowserIsCompatible()) {
-        $scope.map = new GMap2(document.getElementById("map"));
-        $scope.map.setCenter(new GLatLng(57.84701, 12.36555), 11);
-		$scope.map.setMapType(G_HYBRID_MAP);
-		$scope.map.addControl(new GSmallMapControl());				
-			
-		GDownloadUrl("family.json", function(data, responseCode) {
-				//var myval = eval('(' + data + ')');												
-				eval("$scope.family = (" + data + ")");
-
-				//Sort by person id
-				var personIds = new Array();
-				for (var id in $scope.family.persons) {
-						personIds.push(id);
-				}
-				personIds.sort();
-				//Loop through persons
-				personIds.each(function(id) {
-						//map marker
-						var person = $scope.family.persons[id];
-						if (person.placeId && $scope.family.places && $scope.family.places[person.placeId]) {
-							 var marker = createGMarker(person, family.places[person.placeId]);
-							 $scope.map.addOverlay(marker);
-							 $scope.markers[id] = marker;									 
-						}
-						//menu item
-						$('menuitems').innerHTML += "<li><a href=\"#\" onClick=\"JavaScript:displayPerson('" + id + "');\">" + person.name + "</a></li>";
-				});						
-		});
-	}
+	});
 	
     function createGMarker(person, place) {
         var point = new GLatLng(parseFloat(place.lat), parseFloat(place.lon));
@@ -73,8 +55,8 @@ function GMapCtrl($scope, $http) {
 		if (p.mobile) {
 			 text += "<i>mobilnr: " + p.mobile + "</i> ";
 		}				
-		if (family.places[p.placeId]&& family.places[p.placeId].address) {
-			 text += "<br/><a href='http://kartor.eniro.se/query?streetname=" + family.places[p.placeId].address + "&what=map&asearch=1'>" + family.places[p.placeId].address + "</a>";				}				
+		if ($scope.places[p.placeId]&& $scope.places[p.placeId].address) {
+			 text += "<br/><a href='http://kartor.eniro.se/query?streetname=" + $scope.places[p.placeId].address + "&what=map&asearch=1'>" + $scope.places[p.placeId].address + "</a>";				}				
 		text += "<br/>";				
 		if (p.url) {
 			 text += "<i><a href='" + p.url + "'>" + p.url + "</a></i>";
@@ -107,25 +89,29 @@ function GMapCtrl($scope, $http) {
 		return text;		
 	}
 
-	function displayPerson(id) {
-		 if (family.persons[id].image) {				
-  		 $('frontphoto').src = family.persons[id].image;
-		 } else {
-		 	 $('frontphoto').src = 'img/vidvattnet.jpg';
-		 }
-		 if (family.persons[id].quote) {				
-  		 $('quote').innerHTML = family.persons[id].quote;
-		 } else {
-		 	 $('quote').innerHTML = '\"I put on my robe and wizard hat\"<br />(BloodNinja)';
-		 }						 
-		 if (markers[id]) {
-		 map.panTo(markers[id].getPoint());		
-		 GEvent.trigger(markers[id], "click");						 
-		 }
-	}
-    */
 }
 
+function displayPerson(name) {
+    $scope.persons.each(function(p) {
+        if (p.name == name) {
+        	 if (p.image) {				
+      		 $('frontphoto').src = p.image;
+    		 } else {
+    		 	 $('frontphoto').src =  $scope.person.img;
+    		 }
+    		 if (p.quote) {				
+      		 $('quote').innerHTML = p.quote;
+    		 } else {
+    		 	 $('quote').innerHTML = $scope.person.quote;
+    		 }						 
+    		 if (markers[id]) {
+    		 map.panTo(markers[id].getPoint());		
+    		 GEvent.trigger(markers[id], "click");						 
+    		 }                
+        }
+    });
+}
+    
 function MyCtrl1() {
 }
 MyCtrl1.$inject = [];
